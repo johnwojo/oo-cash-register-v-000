afcf5082f6e0bcb39ced06d1036a40be3672f854
John Wojciechowski <jwojo13@gmail.com>
2017-12-08 19:26:32 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,10 +16,10 @@ attr_accessor :total, :discount, :last_transaction, :items
 
   def add_item(title,price,quantity=1)
     @total = @total + (price*quantity)
-      quantity.times do
-        @items << title
-      end
+    quantity.times do
+      @items << title
     end
+  end
 
 
   def apply_discount
